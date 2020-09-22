a231d6ad546785197342236c3f3c2e171d1aaf24
PrusC <konstantin.prusakov@phystech.edu>
2020-09-22 18:26:27 +0300

    add base.html and bootstrap css and js

--- diyblog/urls.py
+++ diyblog/urls.py
@@ -19,6 +19,6 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog', include('blog.urls')),
+    path('blog/', include('blog.urls')),
     path('', RedirectView.as_view(url='blog/', permanent=True)),
 ]
